d6822c4c5aa582a44f57fd4d15952106d7ff7c73
UFGHex <uglyfatjerk@gmail.com>
2016-04-02 11:13:59 +0200

    Create testSDL.c

---
+++ testSDL.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <SDL.h>
+
+int main(int argc, char *argv[]) {
+	if (SDL_Init(SDL_INIT_VIDEO)) {
+		fprintf(stderr,"Erreur SDL : %s\n",SDL_GetError());
+	}
+	/* Surface principale */
+	SDL_Surface *fenetre = NULL;
+	/* Boucle */
+	while(1) {
+		fenetre = SDL_SetVideoMode(500,500,32,SDL_HWSURFACE);
+	}	
+	SDL_FreeSurface(fenetre);
+	/* on quitte et vide la mémoire */	
+	SDL_Quit(); 
+	return 0;
+}
